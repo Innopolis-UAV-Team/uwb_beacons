e7d7ef5067484edc9a3f59e0c5e1881d54b8ac16
Anastasiia Stepanova <asiiapine@gmail.com>
2025-09-05 10:56:23 +0300

    Logs messages (#1)
    * pretty logs in check.py
    
    * pretty logging and more arguments lists for python scripts

--- test/check.py
+++ test/check.py
@@ -1,4 +1,7 @@
+import datetime
 import time
+from typing import Dict
+from numpy import mean
 import serial
 import struct
 import argparse
@@ -17,28 +20,55 @@ def check_ttl_serial(port: str, baudrate: int = 9600, timeout: float = None):
         # Initialize serial connection
         ser = serial.Serial(port, baudrate, timeout=timeout, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, parity="E")
         print(f"Connected to {port} at {baudrate} baud.")
-        n = 0
         # Test data to send
         buffer: CircularBuffer = CircularBuffer(100)
+        messages: Dict[int, dict] = {}
+        last_message = time.time()
+
         while True:
             # Try decoding with error handling
             if ser.in_waiting > 0:
                 try:
                     response = ser.read_until(b'\xFF\x00')
                     buffer.append(response)
-                    if buffer.size != 0:
-                        last_message = time.time()
-                    else :
+                    if buffer.size == 0:
                         continue
                     for i in range(buffer.size):
                         msg = buffer.pop()
                         if msg is None:
                             continue
-                        print(Message(msg))
-                        n+=1
-                    if (n > 100):
-                        print(f"Time: {time.time() - last_message}")
-                        n = 0
+                        # print(Message(msg))
+                        message = Message(msg)
+                        if message.id not in messages:
+                            messages[message.id] = {}
+                            messages[message.id]['data'] = []
+                        if message.data is None:
+                            continue
+                        assert isinstance(message.data, int)
+                        messages[message.id]['data'].append(message.data)
+                        messages[message.id]['last_message'] = time.time()
+
+                    if time.time() - last_message < 1:
+                        continue
+                    last_message = time.time()
+                    # all_messages_str = ""
+                    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                    row = {}
+
+                    for id, data in messages.items():
+                        if time.time() - data['last_message'] > 10:
+                            data['data'] = []
+                            continue
+                        row[f"{id}_pts"] = str(len(data['data']))
+                        row[f"{id}_mean"] = str(int(mean(data['data']))) if len(data['data']) > 0 else "N/A"
+                        messages[id]['data'] = []
+                    # sort the keys
+                    keys = sorted(row.keys())
+                    srting = f"ts: {ts}\t"
+                    for i, key in enumerate(keys):
+                        srting += f"{key}: {row[key]}\t"
+                    print(srting)
                 except struct.error as e:
                     print(e)
                     print(response)
@@ -55,8 +85,8 @@ def check_ttl_serial(port: str, baudrate: int = 9600, timeout: float = None):
 # Example usage
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description='Save data from the TTL to serial converter.')
-    argparser.add_argument('--port', type=str, default='/dev/ttyUSB0', help='The serial port to which the TTL converter is connected (e.g., \'COM3\' on Windows or \'/dev/ttyUSB0\' on Linux).')
-    argparser.add_argument('--baudrate', type=int, default=460800  , help='The baud rate for communication. Default is 9600.')
+    argparser.add_argument('-p', '--port', type=str, default='/dev/ttyUSB0', help='The serial port to which the TTL converter is connected (e.g., \'COM3\' on Windows or \'/dev/ttyUSB0\' on Linux).')
+    argparser.add_argument('-b', '--baudrate', type=int, default=460800  , help='The baud rate for communication. Default is 9600.')
 
     args = argparser.parse_args()
     check_ttl_serial(port=args.port, baudrate=args.baudrate)
--- test/save_test_data.py
+++ test/save_test_data.py
@@ -9,21 +9,9 @@ from common.serial_messages import Message, CircularBuffer, decode
 
 num_entries = 0
 
-def save_to_file(string: str, file: str) -> Tuple[int, float]:
-    messages = decode(string)
-    data = ""
-    for m in messages:
-        global num_entries
-        num_entries += 1
-        if num_entries > 1000:
-            raise Exception("Got nessecary amount of data")
-        data += f"{m}\n"
-    with open(file, 'a') as f:
-        f.write(data)
-        f.close()
 
-def connect_and_save(port: str, baudrate: int = 9600, timeout: float = None,
-                        file_name: str = None, num_points: int = 1000):
+def connect_and_save(port: str, baudrate: int = 9600, timeout: float|None = None,
+                        file_name: str = "output.txt", num_points: int = 1000):
     """
     Check the functionality of a TTL to serial converter.
 
@@ -32,7 +20,7 @@ def connect_and_save(port: str, baudrate: int = 9600, timeout: float = None,
     :param timeout: Read timeout value. Default is 1 second.
     """
     # open file to store results
-    response = ""
+    response: bytes = b''
     try:
         # Initialize serial connection
         ser = serial.Serial(port, baudrate, timeout=timeout, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, parity="E")
@@ -40,6 +28,7 @@ def connect_and_save(port: str, baudrate: int = 9600, timeout: float = None,
         max_size = 100
         buffer: CircularBuffer = CircularBuffer(max_size)
         point_count = 0
+        last_info_time = time.time()
         while True:
             # Try decoding with error handling
             if ser.in_waiting > 0:
@@ -57,11 +46,16 @@ def connect_and_save(port: str, baudrate: int = 9600, timeout: float = None,
                                     return
 
                                 msg = buffer.pop()
+                                if msg is None:
+                                    continue
                                 f.write(str(Message(msg)) + '\n')
                             f.close()
                 except struct.error as e:
                     print(e)
                     print(response)
+            if time.time() - last_info_time > 1:
+                last_info_time = time.time()
+                print(f"Received {point_count}\tpoints out of {num_points}")
 
     except serial.SerialException as e:
         print(f"Serial exception: {e}")
@@ -77,20 +71,24 @@ def connect_and_save(port: str, baudrate: int = 9600, timeout: float = None,
 # Example usage
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description='Save data from the TTL to serial converter.')
-    argparser.add_argument('--port', type=str, default='/dev/ttyUSB0', help='The serial port to which the TTL converter is connected (e.g., \'COM3\' on Windows or \'/dev/ttyUSB0\' on Linux).')
-    argparser.add_argument('--baudrate', type=int, default=230400, help='The baud rate for communication. Default is 230400.')
-    argparser.add_argument('--num_points', type=int, default=1000, help='Number of points to collect before stopping. Default is 1000.')
-    
+    argparser.add_argument('-p', '--port', type=str, default='/dev/ttyUSB0', help='The serial port to which the TTL converter is connected (e.g., \'COM3\' on Windows or \'/dev/ttyUSB0\' on Linux).')
+    argparser.add_argument('-b', '--baudrate', type=int, default=230400, help='The baud rate for communication. Default is 230400.')
+    argparser.add_argument('-n', '--num_points', type=int, default=1000, help='Number of points to collect before stopping. Default is 1000.')
+    argparser.add_argument('-d', '--out_dir', type=str, default='data', help='Directory to save the data to. Default is "data".')
+
     args = argparser.parse_args()
 
     file_name = f'{args.num_points}_{time.strftime("%Y-%m-%d_%H-%M-%S")}.txt'
     # get the current file directory
 
+    if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), args.out_dir)):
+        print(f"Data directory {os.path.join(os.path.dirname(os.path.realpath(__file__)), args.out_dir)} exists.")
+    else:
+        os.mkdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), args.out_dir))
     file_dir = os.path.dirname(os.path.realpath(__file__))
 
-    full_path = os.path.join(file_dir, "data", file_name)
-    print(f"File directory: {file_dir}")
-
+    full_path = os.path.join(file_dir, args.out_dir, file_name)
+    print(f"The output file will be saved to {full_path}")
     # Replace 'COM3' with the appropriate port for your environment
     connect_and_save(port=args.port, baudrate=args.baudrate,
                         file_name=full_path, num_points=args.num_points)
