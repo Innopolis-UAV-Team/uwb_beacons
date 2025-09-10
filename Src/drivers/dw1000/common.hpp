83434b4eec30eca3289df5b08aa2b7e286d4bbb2
AsiiaPine <asiiapine@gmail.com>
2025-09-10 11:58:19 +0300

    simple iqr setup

---
+++ Src/drivers/dw1000/common.hpp
@@ -0,0 +1,66 @@
+#ifndef SRC_DRIVERS_DW1000_COMMON_HPP_
+#define SRC_DRIVERS_DW1000_COMMON_HPP_
+
+/* Length of the common part of the message
+(up to and including the function code,see NOTE 1 below). */
+#define ALL_MSG_COMMON_LEN 10
+/* Buffer to store received response message.
+ * Its size is adjusted to longest frame that this code is supposed to handle. */
+#define RX_BUF_LEN 20
+
+#define ALL_MSG_SN_IDX 2
+#define FINAL_MSG_POLL_TX_TS_IDX 10
+#define FINAL_MSG_RESP_RX_TS_IDX 14
+#define FINAL_MSG_FINAL_TX_TS_IDX 18
+#define FINAL_MSG_TS_LEN 4
+
+/* Preamble timeout, in multiple of PAC size. See NOTE 2 below. */
+#define PRE_TIMEOUT 8
+/* Delay between frames, in UWB microseconds. See NOTE 3 below. */
+
+/* This is the delay from the end of the frame transmission to
+the enable of the receiver, as programmed for the DW1000's wait for response feature. */
+#define POLL_TX_TO_RESP_RX_DLY_UUS 300
+/* This is the delay from Frame RX timestamp to TX reply timestamp used for calculating/setting the DW1000's delayed TX function. This includes the
+ * frame length of approximately 2.66 ms with above configuration. */
+#define RESP_RX_TO_FINAL_TX_DLY_UUS 3100
+/* Receive response timeout. See NOTE 5 below. */
+#define RESP_RX_TIMEOUT_UUS 2700
+#endif  // SRC_DRIVERS_DW1000_COMMON_HPP_
+
+/*
+* 1. The messages here are similar to those used in the DecaRanging ARM application (shipped with EVK1000 kit). They comply with the IEEE
+ *    802.15.4 standard MAC data frame encoding and they are following the ISO/IEC:24730-62:2013 standard. The messages used are:
+ *     - a poll message sent by the initiator to trigger the ranging exchange.
+ *     - a response message sent by the responder allowing the initiator to go on with the process
+ *     - a final message sent by the initiator to complete the exchange and provide all information needed by the responder to compute the
+ *       time-of-flight (distance) estimate.
+ *    The first 10 bytes of those frame are common and are composed of the following fields:
+ *     - byte 0/1: frame control (0x8841 to indicate a data frame using 16-bit addressing).
+ *     - byte 2: sequence number, incremented for each new frame.
+ *     - byte 3/4: PAN ID (0xDECA).
+ *     - byte 5/6: destination address, see NOTE 3 below.
+ *     - byte 7/8: source address, see NOTE 3 below.
+ *     - byte 9: function code (specific values to indicate which message it is in the ranging process).
+ *    The remaining bytes are specific to each message as follows:
+ *    Poll message:
+ *     - no more data
+ *    Response message:
+ *     - byte 10: activity code (0x02 to tell the initiator to go on with the ranging exchange).
+ *     - byte 11/12: activity parameter, not used here for activity code 0x02.
+ *    Final message:
+ *     - byte 10 -> 13: poll message transmission timestamp.
+ *     - byte 14 -> 17: response message reception timestamp.
+ *     - byte 18 -> 21: final message transmission timestamp.
+ *    All messages end with a 2-byte checksum automatically set by DW1000.
+ * 2. The preamble timeout allows the receiver to stop listening in situations where preamble is not starting (which might be because the responder is
+ *    out of range or did not receive the message to respond to). This saves the power waste of listening for a message that is not coming. We
+ *    recommend a minimum preamble timeout of 5 PACs for short range applications and a larger value (e.g. in the range of 50% to 80% of the preamble
+ *    length) for more challenging longer range, NLOS or noisy environments.
+* 3. Delays between frames have been chosen here to ensure proper synchronisation of transmission and reception of the frames between the initiator
+ *    and the responder and to ensure a correct accuracy of the computed distance. The user is referred to DecaRanging ARM Source Code Guide for more
+ *    details about the timings involved in the ranging process.
+ * 4. 5. This timeout is for complete reception of a frame, i.e. timeout duration must take into account the length of the expected frame. Here the value
+ *    is arbitrary but chosen large enough to make sure that there is enough time to receive the complete response frame sent by the responder at the
+ *    110k data rate used (around 3 ms).
+*/
